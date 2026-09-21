a22f508913e56647deadae77068b4016e1505a3c
michael-bailey <bailey-michael1@outlook.com>
2026-09-21 21:02:11 +0100

    create(test): Created linked list implementation tests

---
+++ src/test/kotlin/structures/list/LinkedListTest.kt
@@ -0,0 +1,14 @@
+package structures.list
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class LinkedListTest {
+
+	@Test
+
+
+	@Test
+	fun `can add element`() {}
+
+}
